2dc4b1e5a65d792585fb36b1c22b5642930b7e1b
NicholasC2 <137032388+NicholasC2@users.noreply.github.com>
2026-09-01 02:16:59 +0000

    not workin

---
+++ README.md
@@ -0,0 +1,8 @@
+idk, diy or smthin
+
+build: 
+```sh
+CC="$DEVKITARM/bin/arm-none-eabi-gcc" \
+CXX="$DEVKITARM/bin/arm-none-eabi-g++" \
+meson setup build
+```
--- core/include/defs.h
+++
@@ -1,9 +0,0 @@
-#define CONFIG_PATH "/.steel/config.ini"
-#define THEME_PATH  "/.steel/themes/"
-
-#define DEFAULT_THEME "nebula-blue"
-
-#define DEFAULT_CONFIG "\
-[general]\n\
-theme = \"" DEFAULT_THEME "\"\n\
-"
